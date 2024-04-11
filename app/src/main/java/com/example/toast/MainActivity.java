335db27a7e3dc90358d66d69221489c9d60f55a7
ranajagesh <jageshwarrana25@gmail.com>
2024-04-11 16:47:41 +0530

    first commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button btnToast;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnToast = findViewById(R.id.btnToast);
+
+        btnToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"This Is My First Toast",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+}
